22ebf38e1baaca0c92cbf82070713a82cfa6a72a
renlong567 <renlong567@gmail.com>
2016-01-04 14:22:31 +0800

    清理图标图片，更新备份

--- config.php
+++ config.php
@@ -6,11 +6,16 @@
 if(!defined("PHPOK_SET")){exit("<h1>Access Denied</h1>");}
 // 连接数据库引挈，当前配置仅适用于小站点使用
 $config["db"]["file"] = "mysqli";
-$config["db"]["host"] = "127.0.0.1";
+$config["db"]["host"] = "localhost";
 $config["db"]["port"] = "3306";
 $config["db"]["user"] = "root";
 $config["db"]["pass"] = "root";
 $config["db"]["data"] = "yurenhigh";
+// $config["db"]["host"] = "qdm170119463.my3w.com";
+// $config["db"]["port"] = "3306";
+// $config["db"]["user"] = "qdm170119463";
+// $config["db"]["pass"] = "3152983";
+// $config["db"]["data"] = "qdm170119463_db";
 $config["db"]["prefix"] = "qinggan_";
 $config['db']['socket'] = '';
 $config['db']['debug'] = false;
@@ -22,10 +27,10 @@ $config['db']['cache']['port'] = 11211;
 $config['db']['cache']['time'] = 86400; //Memcache限制不能超过30天，我们建议设置86400，一天
 
 //手机端配置
-$config['mobile']['autocheck'] = true; //自动检测手机端，启用后，检测出手机端将读取手机端网页
-$config['mobile']['status'] = true; //手机端开始，此项不开启的话，将不使用手机
+$config['mobile']['autocheck'] = false; //自动检测手机端，启用后，检测出手机端将读取手机端网页
+$config['mobile']['status'] = false; //手机端开始，此项不开启的话，将不使用手机
 $config['mobile']['default'] = false; //默认为手机版，为方便开发人员调式，设置为默认后，在网页上也会展示手机版
 
-$config['develop'] = true; //开发模式，正常运行的网站请设为false，可防止CRSF注入
-$config['debug'] = true;
+$config['develop'] = false; //开发模式，正常运行的网站请设为false，可防止CRSF注入
+$config['debug'] = false;
 ?>
